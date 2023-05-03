72a00911a34bbca5115533a675e7d4598d9cf124
nik.volkov <playpaper12@gmail.com>
2023-05-04 01:50:57 +0300

    refactor createBitAccessor

--- src/createBitAccessor/createBitAccessor.ts
+++ src/createBitAccessor/createBitAccessor.ts
@@ -1,4 +1,4 @@
-type BitIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
+export type BitIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
 type Bit = 0 | 1
 
 export class BitAccessor {
@@ -8,20 +8,20 @@ export class BitAccessor {
         this.#bitArr = bitArr
     }
 
-    get(biteIndex: number, bitIndex: BitIndex) {
-        const bite = this.#bitArr[biteIndex]
+    get(byteIndex: number, bitIndex: BitIndex) {
+        const bite = this.#bitArr[byteIndex]
         const mask = 1 << bitIndex
         const bit = (bite & mask) >> bitIndex
 
         return bit
     }
 
-    set(biteIndex: number, bitIndex: BitIndex, bitToSet: Bit) {
-        if (this.get(biteIndex, bitIndex) !== bitToSet){
-            const bite = this.#bitArr[biteIndex]
+    set(byteIndex: number, bitIndex: BitIndex, bitToSet: Bit) {
+        if (this.get(byteIndex, bitIndex) !== bitToSet){
+            const bite = this.#bitArr[byteIndex]
             const mask = 1 << bitIndex
 
-            this.#bitArr[biteIndex] = bite ^ mask
+            this.#bitArr[byteIndex] = bite ^ mask
         }
     }
 }
